689be6c864bf344bbccb577e16290c3560659093
sudaya02 <sujee30@hotmail.com>
2015-08-26 18:55:58 +1000

    Made fixes discovered in code review

--- ITC515_assignment2/src/datamanagement/AppProperties.java
+++ ITC515_assignment2/src/datamanagement/AppProperties.java
@@ -2,13 +2,29 @@ package datamanagement;
 import java.util.*;
 import java.io.*;
 public class AppProperties {
-private static AppProperties self = null;
+private static AppProperties instance_ = null;
 private Properties properties;
 
 public static AppProperties getInstance() {
-if (self == null ) { self = new AppProperties(); } return self;}
-private AppProperties() {properties = new Properties();
-try {properties.load(new FileInputStream("Properties.prop"));} 
-catch (IOException e) {throw new RuntimeException("Could not read property file");}}
-    public Properties getProperties() {return properties;
-}}
+//Incorrect lazy initialization of static field    
+if (instance_ == null ) {
+    instance_ = new AppProperties(); 
+} 
+return instance_;
+}
+
+private AppProperties() {
+    properties = new Properties();
+    try {
+        properties.load(new FileInputStream("Properties.prop"));
+        //Method may fail to clean up stream or resource
+        //Method may fail to close stream
+    } 
+    catch (IOException e) {
+        throw new RuntimeException("Could not read property file");
+    }
+}
+    public Properties getProperties() {
+        return properties;
+    }
+}

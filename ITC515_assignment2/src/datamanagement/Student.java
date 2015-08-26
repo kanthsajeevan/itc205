ad7dff1077ed212a2f24fd5e311ad8449d83f9f2
skling02 <klingershehani@ymail.com>
2015-08-27 06:38:26 +1000

    Update Student.java

--- ITC515_assignment2/src/datamanagement/Student.java
+++ ITC515_assignment2/src/datamanagement/Student.java
@@ -5,34 +5,62 @@ public class Student implements IStudent {
             private String ln;
 private StudentUnitRecordList su;
 
+// Constructor for the Student class. Will create the Student object based
+// on the values of the parameters passed.
 public Student( Integer id, String fn, String ln, StudentUnitRecordList su ) { this.id = id; this.fn = fn;
         this.ln = ln;this.su = 
-        su == null ? new StudentUnitRecordList() : 
+       
+                su == null ? new StudentUnitRecordList() : 
                 su;
 }
 
     public Integer getID() { return this.id; 
-} public String getFirstName() { 
-return fn; }
+
+    } 
+    public String getFirstName() { 
+
+        return fn; }
 
     public void setFirstName( String firstName ) { 
-this.fn = firstName; }
 
-public String getLastName() { 
-    return ln; }
+        this.fn = firstName; }
+
+
+    public String getLastName() { 
+    
+        return ln; }
+   
     public void setLastName( String lastName ) { 
 
         
-this.ln = lastName; }
 
-public void addUnitRecord( IStudentUnitRecord record ) { su.add(record); }
+        this.ln = lastName; }
+    
+     // The addUnitRecord method will add the passed student unit record
+  
+     // to this object's student unit record list.
+
+
+    public void addUnitRecord( IStudentUnitRecord record ) { su.add(record); }
         public IStudentUnitRecord getUnitRecord( String unitCode ) {
-for ( IStudentUnitRecord r : su ) 
-            if ( r.getSubjectCode().equals(unitCode)) 
-return r; 
 
-return null;
+            for ( IStudentUnitRecord r : su ) 
+            
+                if ( r.getUnitCode().equals(unitCode)) 
+
+                
+                    return r; 
+
+
+            
+            return null;
         
 }
+        // The getUnitRecord method differs from the standard getter that
+        // returns the entire unit record list, rather it returns one unit
+        // record, if it exists, based on a passed unit code.
 
-public StudentUnitRecordList getUnitRecords() { return su; }}
+        public StudentUnitRecordList getUnitRecords() {
+            return su; 
+        }
+}

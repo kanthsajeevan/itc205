7e870ffff58a4f990b180b056f74d26a2f3cbc14
skling02 <klingershehani@ymail.com>
2015-08-26 17:42:57 +1000

    Update StudentUnitRecord.java
    Fixed a error:
    StudentUnitRecord is not abstract and does not override abstract method getTotalMark() in IStudentUnitRecord
    
    Changed variable:
    asg1 -> assignment1, float asg2 -> assignment2,
    Changed method:
    setAsg1() -> setAssignment1(), setAsg2() -> setAssignment2(),

--- ITC515_assignment2/src/datamanagement/StudentUnitRecord.java
+++ ITC515_assignment2/src/datamanagement/StudentUnitRecord.java
@@ -1,16 +1,16 @@
 package datamanagement;
 
 public class StudentUnitRecord implements IStudentUnitRecord {
-	private Integer sid;
-	private String uc;
-	private float a1, a2, ex;
+	private Integer sid; //variable to store the student ID
+	private String uc;   //variable to store the unit code
+	private float a1, a2, ex; //variables to store assignment 1, assignment2 and exam marks
 
-	public StudentUnitRecord(Integer id, String code, float asg1, float asg2,
+	public StudentUnitRecord(Integer id, String code, float assignment1, float assignment2,
 			float exam) {
 		this.sid = id;
 		this.uc = code;
-		this.setAsg1(asg1);
-		this.setAsg2(asg2);
+		this.setAssignment1(assignment1);
+		this.setAssignment2(assignment2);
 		this.setExam(exam);
 	}
 
@@ -22,7 +22,9 @@ public class StudentUnitRecord implements IStudentUnitRecord {
 		return uc;
 	}
 
-	public void setAsg1(float a1) {
+	public void setAssignment1(float a1) {
+            
+           /// Check assignment 1 marks
 		if (a1 < 0 ||
 			a1 > UnitManager.UM().getUnit(uc).getAsg1Weight()) {
 			throw new RuntimeException("Mark cannot be less than zero or greater than assessment weight");
@@ -30,12 +32,13 @@ public class StudentUnitRecord implements IStudentUnitRecord {
 		this.a1 = a1;
 	}
 
-	public float getAsg1() {
+	public float getAssignment1() {
 
 		return a1;
 	}
 
-	public void setAsg2(float a2) {
+	public void setAssignment2(float a2) {
+            // Check assignment 2 marks
 		if (a2 < 0 ||
 			a2 > UnitManager.UM().getUnit(uc).getAsg2Weight()) {
 			throw new RuntimeException("Mark cannot be less than zero or greater than assessment weight");
@@ -44,11 +47,12 @@ public class StudentUnitRecord implements IStudentUnitRecord {
 
 	}
 
-	public float getAsg2() {
+	public float getAssignment2() {
 		return a2;
 	}
 
 	public void setExam(float ex) {
+            // Check exam marks
 		if (ex < 0 ||
 				ex > UnitManager.UM().getUnit(uc).getExamWeight()) {
 				throw new RuntimeException("Mark cannot be less than zero or greater than assessment weight");
@@ -61,7 +65,23 @@ public class StudentUnitRecord implements IStudentUnitRecord {
 	}
 
 	public float getTotal() {
+            //Return the total mark
 		return a1 + a2 + ex;
 
 	}
+
+    @Override
+    public Integer getStudentId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getSubjectCode() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public float getTotalMark() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }

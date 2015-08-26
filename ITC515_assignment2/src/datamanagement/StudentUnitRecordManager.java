f8f5d73ae3f835c3bba2a1d8e9956a4ba73dffde
skling02 <klingershehani@ymail.com>
2015-08-26 17:27:27 +1000

    Update StudentUnitRecordManager.java
    Fixed a error:
    <anonymous datamanagement.StudentUnitRecordManager$1> is not abstract and does not override abstract method setAssignment1(float) in IStudentUnitRecord
    by adding:
     @Override
            public void setAssignment1(float assignment1Mark) {
                throw new UnsupportedOperationException("Not supported yet."); 
            }
    changed method name  getStudentID() -> getStudentId(), getUnitCode -> getSubjectCode(), getAsg1() -> getAssignment1(),getAsg2() -> getAssignment2()

--- ITC515_assignment2/src/datamanagement/StudentUnitRecordManager.java
+++ ITC515_assignment2/src/datamanagement/StudentUnitRecordManager.java
@@ -6,14 +6,22 @@ import org.jdom.*;
 public class StudentUnitRecordManager {
 
 private static StudentUnitRecordManager s = null;
-            private StudentUnitRecordMap rm;
-    private java.util.HashMap<String,StudentUnitRecordList> ur;
-private java.util.HashMap<Integer,StudentUnitRecordList> sr;
+            private final StudentUnitRecordMap rm;
+    private final java.util.HashMap<String,StudentUnitRecordList> ur;
+private final java.util.HashMap<Integer,StudentUnitRecordList> sr;
     public static StudentUnitRecordManager instance() {
-        if (s == null ) s = new StudentUnitRecordManager(); return s;}
+ 
+        if (s != null ) {
+        } else {
+            s = new StudentUnitRecordManager();
+        } return s;}
+    
             private StudentUnitRecordManager() {
+                // create StudentUnitRecordMap
         rm = new StudentUnitRecordMap();
+         // create hash map
     ur = new java.util.HashMap<>();
+     // create hash map
     sr = new java.util.HashMap<>();}
     public IStudentUnitRecord getStudentUnitRecord( Integer studentID, String unitCode ) {
 IStudentUnitRecord ir = rm.get(studentID.toString()+unitCode);
@@ -23,7 +31,8 @@ return ir != null ? ir : createStudentUnitRecord(studentID, unitCode);}
         IStudentUnitRecord ir;
         for (Element el : (List<Element>) XMLManager.getXML().getDocument().getRootElement().getChild("studentUnitRecordTable").getChildren("record")) {
         if (uid.toString().equals(el.getAttributeValue("sid")) && sid.equals(el.getAttributeValue("uid"))) {
-                ir = new StudentUnitRecord( new Integer(el.getAttributeValue("sid")),el.getAttributeValue("uid"),new Float(el.getAttributeValue("asg1")).floatValue(),new Float(el.getAttributeValue("asg2")).floatValue(),new Float(el.getAttributeValue("exam")).floatValue() );
+     
+            ir = new StudentUnitRecord(  Integer.valueOf(el.getAttributeValue("sid")),el.getAttributeValue("uid"),Float.parseFloat(el.getAttributeValue("asg1")), Float.valueOf(el.getAttributeValue("asg2")),Float.parseFloat(el.getAttributeValue("exam")) );
                rm.put(ir.getStudentId().toString()+ir.getSubjectCode(), ir);return ir;
 }
 }
@@ -33,7 +42,13 @@ throw new RuntimeException("DBMD: createStudent : student unit record not in fil
     if ( recs != null ) return recs; 
         recs = new StudentUnitRecordList();
         for (Element el : (List<Element>) XMLManager.getXML().getDocument().getRootElement().getChild("studentUnitRecordTable").getChildren("record")) {
-    if (unitCode.equals(el.getAttributeValue("uid"))) recs.add(new StudentUnitRecordProxy( new Integer(el.getAttributeValue("sid")), el.getAttributeValue("uid")));
+    if (unitCode.equals(el.getAttributeValue("uid"))) recs.add(new StudentUnitRecordProxy( new Integer(el.getAttributeValue("sid")), el.getAttributeValue("uid")) {
+
+        @Override
+        public void setAssignment1(float assignment1Mark) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    });
         }
         if ( recs.size() > 0 ) 
             ur.put(unitCode, recs); // be careful - this could be empty
@@ -45,24 +60,32 @@ public StudentUnitRecordList getRecordsByStudent( Integer studentID ) {
     if ( recs != null ) return recs; recs = new StudentUnitRecordList();
         for (Element el : (List<Element>) XMLManager.getXML().getDocument().getRootElement().getChild("studentUnitRecordTable").getChildren("record")) 
             if (studentID.toString().equals(el.getAttributeValue("sid"))) 
-                recs.add(new StudentUnitRecordProxy( new Integer(el.getAttributeValue("sid")), el.getAttributeValue("uid")));
+                recs.add(new StudentUnitRecordProxy( new Integer(el.getAttributeValue("sid")), el.getAttributeValue("uid")) {
+
+        @Override
+        public void setAssignment1(float assignment1Mark) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    });
                 if ( recs.size() > 0 ) 
                     sr.put(studentID, recs); // be careful - this could be empty
                         return recs;
     }
-
+    /** save the recor
+     * @param irec */
     public void saveRecord( IStudentUnitRecord irec ) {
         for (Element el : (List<Element>) XMLManager.getXML().getDocument().getRootElement().getChild("studentUnitRecordTable").getChildren("record")) {
-            if (irec.getStudentID().toString().equals(el.getAttributeValue("sid")) && irec.getUnitCode().equals(el.getAttributeValue("uid"))) {
-                el.setAttribute("asg1", new Float(irec.getAsg1()).toString());
-                
-                
-                
-        el.setAttribute("asg2", new Float(irec.getAsg2()).toString());
-        el.setAttribute("exam", new Float(irec.getExam()).toString());
+            if (irec.getStudentId().toString().equals(el.getAttributeValue("sid")) && irec.getSubjectCode().equals(el.getAttributeValue("uid"))) {
+                Element setAttribute;
+                setAttribute = el.setAttribute("asg1", Float.toString(irec.getAssignment1()));
+                Element setAttribute1;
+               setAttribute1 = el.setAttribute("asg2", Float.toString(irec.getAssignment2()));
+
+                Element setAttribute2 = el.setAttribute("exam", Float.toString(irec.getExam()) );
         XMLManager.getXML().saveDocument(); //write out the XML file for continuous save
         return;
-}}
+}
+        }
         
         
         

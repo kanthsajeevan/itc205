b79bb17329476272a1ae4ca032156045fa43038c
skling02 <klingershehani@ymail.com>
2015-08-27 17:53:56 +1000

    Update ListStudentsCTL.java

--- ITC515_assignment2/src/datamanagement/ListStudentsCTL.java
+++ ITC515_assignment2/src/datamanagement/ListStudentsCTL.java
@@ -1,8 +1,22 @@
 package datamanagement;
         public class ListStudentsCTL {
-private StudentManager sm;
-public ListStudentsCTL() {sm = StudentManager.get();}
-            public void listStudents( IStudentLister lister, String unitCode ) {
-    lister.clearStudents();
-                StudentMap students = sm.getStudentsByUnit( unitCode );
-for (Integer id : students.keySet() ) lister.addStudent(students.get(id));}}
+            
+private StudentManager studentManager;
+
+// Constructor for the ListStudentsCTL class. It retrieves the
+// StudentManager instance.
+public ListStudentsCTL() {
+    studentManager = StudentManager.getInstance();
+}
+            
+ // The listStudents method will take a list of students (studentLister) 
+ // and initially empty it. It then adds students to the studentLister based 
+ // on which ones do the unit that matches the passed unitCode parameter.
+public void listStudents( IStudentLister studentLister, String unitCode ) {
+    studentLister.clearStudents();
+    
+                StudentMap students = studentManager.getStudentsByUnit( unitCode );
+
+                for (Integer id : students.keySet() ) studentLister.addStudent(students.get(id));
+}
+        }

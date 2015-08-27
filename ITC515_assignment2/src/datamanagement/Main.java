64e7d5f7956c1da3d27b5ad4d482f64b9b9cbade
skling02 <klingershehani@ymail.com>
2015-08-27 17:53:02 +1000

    Update Main.java

--- ITC515_assignment2/src/datamanagement/Main.java
+++ ITC515_assignment2/src/datamanagement/Main.java
@@ -1,4 +1,8 @@
 package datamanagement;
 
 public class Main {
-    public static void main(String[] p) {new cgCTL().execute();}    }
+    public static void main(String[] p) {
+        // Create an instance of cgCTL, i.e. execute the program.
+        new cgCTL().execute();
+    }   
+}

91e17a60c6c114decebdc294b2a8074e63395656
skling02 <klingershehani@ymail.com>
2015-08-27 17:48:25 +1000

    Update AppProperties.java

--- ITC515_assignment2/src/datamanagement/AppProperties.java
+++ ITC515_assignment2/src/datamanagement/AppProperties.java
@@ -6,9 +6,21 @@ private static AppProperties self = null;
 private Properties properties;
 
 public static AppProperties getInstance() {
-if (self == null ) { self = new AppProperties(); } return self;}
-private AppProperties() {properties = new Properties();
-try {properties.load(new FileInputStream("Properties.prop"));} 
-catch (IOException e) {throw new RuntimeException("Could not read property file");}}
-    public Properties getProperties() {return properties;
-}}
+    
+if (self == null ) {
+    self = new AppProperties();
+} 
+return self;
+}
+private AppProperties() {
+    properties = new Properties();
+try {
+    properties.load(new FileInputStream("Properties.prop"));
+} 
+catch (IOException e) {throw new RuntimeException("Could not read property file");
+}
+}
+    public Properties getProperties() {
+        return properties;
+}
+}

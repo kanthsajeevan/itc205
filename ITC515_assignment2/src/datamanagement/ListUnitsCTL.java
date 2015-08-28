7e5b19f3e899ea98fb3fc7bd8615dc2853d064f0
Sajeevan <kanthsajeevan@users.noreply.github.com>
2015-08-28 15:54:07 +1000

    Update ListUnitsCTL.java

--- ITC515_assignment2/src/datamanagement/ListUnitsCTL.java
+++ ITC515_assignment2/src/datamanagement/ListUnitsCTL.java
@@ -1,10 +1,19 @@
 package datamanagement;
 public class ListUnitsCTL {
-    private UnitManager um;
-public ListUnitsCTL() {
-        um = UnitManager.UM();
+    
+    private UnitManager unitManager;
+
+    // Constructor for the ListUnitsCTL class. It retrieves
+    // the UnitManager instance.
+    public ListUnitsCTL() {
+        unitManager = UnitManager.UM();
 }
-            public void listUnits( IUnitLister lister ) {
-lister.clearUnits();UnitMap units = um.getUnits();
-        for (String s : units.keySet() )
-            lister.addUnit(units.get(s));}}
+    
+     // The listUnits method will take a list of units (unitLister) and
+     // initially empty it. It then adds all units to the unitLister.
+    public void listUnits( IUnitLister unitLister ) {
+
+        unitLister.clearUnits();
+        UnitMap units = unitManager.getUnits();
+        for (String unitCode : units.keySet() )
+            unitLister.addUnit(units.get(unitCode));}}
